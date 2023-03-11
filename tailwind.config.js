d2dabd8dc6d5a553474f96f5429b5189ec44a147
hagoromo2000 <revtm000126@gmail.com>
2023-03-11 16:24:09 +0900

    Fix:配色に影響が出るため、Flowbiteの適用を排除

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,11 @@ module.exports = {
     "./src/components/*.{js,ts,jsx,tsx}",
     "./index.js",
     "./src/**/*.{js,ts,jsx,tsx}",
-    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [
-    require("daisyui"),
-    require("@tailwindcss/typography"),
-    require("flowbite/plugin"),
-  ],
+  plugins: [require("daisyui"), require("@tailwindcss/typography")],
   daisyui: {
     themes: ["cupcake"],
   },
